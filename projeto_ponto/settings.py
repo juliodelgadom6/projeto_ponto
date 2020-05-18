fe9373b8fc243c0eb3cd323cbe69505c4e4d8c5a
Julio Delgado <juliom6@gmail.com>
2020-05-17 21:50:11 -0300

    adicionando templates

--- projeto_ponto/settings.py
+++ projeto_ponto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # nossa applicacao
+    'paginas.apps.PaginasConfig'
 ]
 
 MIDDLEWARE = [
