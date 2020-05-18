e187c4b07347f98c388e1018fa90d4e03c9b092a
Julio Delgado <juliom6@gmail.com>
2020-05-18 00:48:59 -0300

    salvando apotamento e trazendo ultimos 10

--- paginas/models.py
+++ paginas/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from datetime import datetime
-# Create your models here.
+
+
 class Apontamento(models.Model):
     nome_usuario = models.CharField(max_length=25, null=True, blank=False)
     hora = models.DateTimeField(default=datetime.now, blank=False)
+
+    @property
+    def serializar(self):
+        return {
+            'id': self.id,
+            'nome_usuario': self.nome_usuario,
+            'hora': str(self.hora)[11:16]
+        }
--- paginas/views.py
+++ paginas/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html', {})
+
+
+def funcionarios(request):
+    if request.method == 'POST':
+        print(request.POST['nome']) # a pessoa digitou
+        usuario = Apontamento.objects.create(nome_usuario=request.POST['nome'])
+        usuario.save()
+        return render(request, 'index.html', {})
+
+    if request.method == 'GET':
+        ultimos10 = Apontamento.objects.order_by(-Apontamento.id)[:10]
+        # print(ultimos10)
+        # j = jsonify(funcionarios=[i.serializar for i in ultimos10])
+        # print(j)
+        # return j
+        return JsonResponse({'funcionarios': [i.serializar for i in ultimos10]}, safe=False)
