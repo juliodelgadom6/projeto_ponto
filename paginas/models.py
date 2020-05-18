07bac2778d3b4e22b8f216c753b4cf80b890f624
Julio Delgado <juliom6@gmail.com>
2020-05-17 22:25:05 -0300

    adicionado o modelo Apontamento

--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Apontamento(models.Model):
+    nome_usuario = models.CharField(max_length=25, null=True, blank=False)
+    hora = models.DateTimeField(default=datetime.now, blank=False)
